ccd671d9f2fba716fa62b46bf6f16245d37075d0
unknown <mart.mar@gmail.com>
2017-10-19 12:36:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+curso
+=====
+
+A Symfony project created on October 19, 2017, 8:20 am.
---
+++ web/info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ripley
+ * Date: 19/10/2017
+ * Time: 10:39
+ */
+  phpinfo();
+  ?>
